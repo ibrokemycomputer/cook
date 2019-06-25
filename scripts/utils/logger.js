85708e1f7110098fc57858c3fdfe11952a1e76d8
dspint <dsargent@pint.com>
2019-06-25 16:56:07 -0700

    Added default Logger type

--- scripts/utils/logger.js
+++ scripts/utils/logger.js
@@ -11,6 +11,7 @@ let types = {
   error: message => console.log(chalk.red('error'), message),
   header: message => console.log(chalk.grey.underline(message)),
   info: message => console.log(chalk.blue('info'), message),
+  msg: message => console.log(message),
   success: message => console.log(chalk.green('✓ success'), message),
   system: message => console.log(chalk.blue(message)),
   warning: message => console.log(chalk.yellow('warn'), message),
