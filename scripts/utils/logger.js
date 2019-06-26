020e93c9e22950fd378690b24273a94855963934
dspint <dsargent@pint.com>
2019-06-25 17:34:37 -0700

    Added indentation mechanism to Logger

--- scripts/utils/logger.js
+++ scripts/utils/logger.js
@@ -7,19 +7,30 @@ const chalk = require('chalk');
 // ----------------------------------
 let types = {
   break: () => console.log('\n'),
-  done: message => console.log(chalk.magenta('» done') + chalk.gray(` | ${message}`)),
-  error: message => console.log(chalk.red('error'), message),
-  header: message => console.log(chalk.grey.underline(message)),
-  info: message => console.log(chalk.blue('info'), message),
-  msg: message => console.log(message),
-  success: message => console.log(chalk.green('✓ success'), message),
-  system: message => console.log(chalk.blue(message)),
-  warning: message => console.log(chalk.yellow('warn'), message),
+  done: (message, indent=0) => console.log(`${setIndent(indent)}${ chalk.magenta('» done') + chalk.gray(` | ${message}`) }`),
+  error: (message, indent=0) => console.log(`${setIndent(indent)}${ chalk.red('error') }`, message),
+  header: (message, indent=0) => console.log(`${setIndent(indent)}${ chalk.grey.underline(message) }`),
+  info: (message, indent=0) => console.log(`${setIndent(indent)}${ chalk.blue('info') }`, message),
+  msg: (message, indent=0) => console.log(`${setIndent(indent)}${ message }`),
+  success: (message, indent=0) => console.log(`${setIndent(indent)}${ chalk.green('✓ success')}`, message),
+  system: (message, indent=0) => console.log(`${setIndent(indent)}${ chalk.blue(message) }`),
+  warning: (message, indent=0) => console.log(`${setIndent(indent)}${ chalk.yellow('warn') }`, message),
 }
 
 // Mute logging if environment var set
 if (!process.env.LOGGER) Object.keys(types).forEach(key => types[key] = () => ({}));
 
+// HELPER METHODS
+// ----------------------------------
+
+/**
+ * @description Return designated amount of spaces
+ * @param {Number} indent - The number of indents
+ */
+function setIndent(indent) {
+  const indentSrc = '                    ';
+  return indentSrc.substr(0, indent);
+}
 
 // EXPORT
 // ----------------------------------
